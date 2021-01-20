18357495210077cbbecae15cff2e7ee4d3fd0134
Ian Lau <ian840619@gmail.com>
2021-01-21 00:09:03 +0800

    comments

--- practice/common.ts
+++ practice/common.ts
@@ -1,6 +1,7 @@
 namespace common{
 
     function removeDuplicate<T>(arrs: Array<T>): number {
+        // T must be sortable & comparable
         let i: number = 0;
         arrs.sort();
         for (let j = 0; j < arrs.length; j++) {
@@ -15,9 +16,11 @@ namespace common{
 
     let arr = [1,2,3,1,2,5,8,8,7];
     let arr2 = ['c','d','c','f','g','a','c'];
+    let arr3 = ['c','d','c','f','g','a','c',1,2,3,1,2,5,8,8,7];
     console.log(removeDuplicate(arr));
     console.log(arr);
     console.log(removeDuplicate(arr2));
     console.log(arr2);
-
+    console.log(removeDuplicate(arr3));
+    console.log(arr3);
 }
