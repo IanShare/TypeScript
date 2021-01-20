1829d0a3a5c934c3225933959fa9ccae60fef6ad
Ian Lau <ian840619@gmail.com>
2021-01-20 23:59:12 +0800

    Generic Remove Duplicate Function

--- practice/common.ts
+++ practice/common.ts
@@ -1,38 +1,23 @@
 namespace common{
-    let removeDuplicate = function(nums:number[]):number{
-        nums = nums.filter((data,index)=>{
-            return nums.indexOf(data) === index;
-          });
-        console.log(nums);
-        return nums.length;
-    }
 
-    function test(nums:Array<number>):number {
-        nums = new Array<number>();
-        console.log(nums);
-        return nums.length;
-    }
-
-    class MyTest{ 
-        constructor(public nums:Array<number>){
+    function removeDuplicate<T>(arrs: Array<T>): number {
+        let i: number = 0;
+        arrs.sort();
+        for (let j = 0; j < arrs.length; j++) {
+            if(arrs[j] !== arrs[i]) {
+                i++;
+                arrs[i] = arrs[j];
+            }
         }
+        arrs.splice(i+1);
+        return i + 1
     }
 
-    function test2(mytest:MyTest):number {
-        mytest.nums = new Array<number>();
-        console.log(mytest.nums );
-        return mytest.nums.length;
-    }
-    
     let arr = [1,2,3,1,2,5,8,8,7];
+    let arr2 = ['c','d','c','f','g','a','c'];
     console.log(removeDuplicate(arr));
     console.log(arr);
-    let arr2 = new Array<number>(1,2,3,1,2,5,8,8,7);
-    console.log(test(arr2));
+    console.log(removeDuplicate(arr2));
     console.log(arr2);
-    let myTestObj = new MyTest([1,2,3,1,2,5,8,8,7]);
-    
-    console.log(myTestObj.nums);
-    console.log(test2(myTestObj));
-    console.log(myTestObj.nums);
+
 }
